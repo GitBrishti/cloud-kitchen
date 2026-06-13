82d7daf0058879238453fb3f2b499282d6aeb838
GitBrishti <159425432+GitBrishti@users.noreply.github.com>
2026-06-14 02:27:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -20,4 +21,7 @@ def contact():
     return render_template("contact.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    is_development = os.environ.get("RENDER") is None
+    
+    app.run(host="0.0.0.0", port=port, debug=is_development)
